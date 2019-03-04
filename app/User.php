020db27d7c5bb4eaee7ee6e454da7d4ebcc2c68d
Bertuga <bertuga@gmail.com>
2019-03-04 00:16:57 -0300

    constant for user roles added

--- app/User.php
+++ app/User.php
@@ -11,6 +11,9 @@ class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
+    const ROLE_ADMIN = 1;
+    const ROLE_USER = 2;
+
     /**
      * The attributes that are mass assignable.
      *
