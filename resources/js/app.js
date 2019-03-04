242b63ff1213b651060cebb67b58527dfc6a1ac9
Bertuga <bertuga@gmail.com>
2019-03-04 00:24:54 -0300

    Simple role added to register route and custom config in vue-auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
+import './bootstrap'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import axios from 'axios'
@@ -49,16 +50,22 @@ const router = new VueRouter({
 		name: 'register-user',
 		component: RegisterUser,
 		meta: {
-			auth: true
+			auth: {roles: 1}
 		},
 	}]
 });
 
 Vue.router = router
 Vue.use(require('@websanova/vue-auth'), {
-   auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
-   http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
-   router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
+	auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+	http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+	router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
+	rolesVar: 'role',
+	registerData: {url: 'api/auth/register'},
+	loginData: {url: 'api/auth/login'},
+	logoutData: {url: 'api/auth/logout'},
+	fetchData: {url: 'api/auth/user'},
+	refreshData: {url: 'api/auth/refresh'}
 });
 
 const app = new Vue({
@@ -66,7 +73,3 @@ const app = new Vue({
 	components: { App },
 	router,
 });
-
-window.$ = window.jQuery = require('jquery');
-window.Popper = require('popper.js').default;
-require('bootstrap');
