e068a35f83d2b8bf5311ec5f94c3cce74863804a
Bertuga <bertuga@gmail.com>
2019-04-28 18:00:42 -0300

    Title changed

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Sistema</title>
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <meta name="csrf-token" content="{{ csrf_token() }}">
